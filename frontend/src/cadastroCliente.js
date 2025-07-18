641dc93680ed923113f1fb52b2d87b010ffd2461
CaioArgenta <127338893+CaioArgenta@users.noreply.github.com>
2025-07-18 23:29:04 +0000

    Atualiza dependências para versões mais recentes

--- frontend/src/cadastroCliente.js
+++ frontend/src/cadastroCliente.js
@@ -1,4 +1,4 @@
-import React, { use } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
